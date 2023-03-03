ad885cf28f7f29b1e35f886878da6d416b5f6815
Sahil <leviack629@gmail.com>
2023-03-03 18:50:08 +0530

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className='max-w-[1140px] bg-black fixed top-0 left-0 right-0 z-10'>
+            <div className='justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8'>
+
+            </div>
+
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
